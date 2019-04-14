580f8597e47b4a3fee2be2773ebab4f4fa273f83
Giovanni Izzo <giovanni.izzo@i-seed.it>
2019-04-14 07:51:50 +0200

    first version

---
+++ src/storehash.js
@@ -0,0 +1,35 @@
+import web3 from './web3';
+//Your contract address
+const address = '0x99d4ece027ace2895b9d3d07d030532c9d6df53a';
+//Your contract ABI
+const abi = [
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "x",
+                "type": "string"
+            }
+        ],
+        "name": "sendHash",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "getHash",
+        "outputs": [
+            {
+                "name": "x",
+                "type": "string"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    }
+]
+export default new web3.eth.Contract(abi, address);
